22c5bd9558963155ad9762ae5cfe2ca64b62869e
Helene Goyat <goyat.helene@gmail.com>
2024-12-10 17:53:39 -0600

    Re-design

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,17 @@ const selectedBlockPositionList = ref<IPosition[]>([]);
 const solutions = ref<Array<number[]>>([]);
 const viewSolutionIndex = ref<number>(0);
 
-const base = computed(() => new Array(length.value * height.value).fill(0));
 const _base = ref<number[]>([
   2, 4, 4, 4, 4, 11, 11, 11, 9, 9, 9, 2, 2, 0, 4, 0, 0, 0, 11, 9, 12, 9, 2, 0,
   0, 0, 0, 0, 0, 11, 8, 12, 12, 0, 0, 0, 0, 0, 0, 0, 8, 8, 7, 7, 0, 0, 0, 0, 0,
   0, 0, 8, 7, 7, 7,
 ]);
+const base = computed(() => new Array(length.value * height.value).fill(0));
+const selectedBlockBackgroundColor = computed(
+  (): string =>
+    blockList.value.find((b) => b.id === selectedBlockId.value)
+      ?.backgroundColor || "",
+);
 
 function handleSelectBlock(id: number) {
   const block = blockList.value.find((b) => b.id === id);
@@ -55,18 +60,40 @@ function solve() {
   );
   solutions.value = result;
 }
+
+function handlePositionSelect(event: Event, index: number): void {
+  const node = document.getElementById(index.toString());
+  if (!node) return;
+  const clone = node.cloneNode(true);
+
+  const container = document.createElement("div");
+  container.appendChild(clone);
+  console.log(container.firstElementChild?.classList);
+  container.style.position = "absolute";
+  container.style.top = "10px";
+  container.style.left = "10px";
+
+  container.firstElementChild?.classList.remove("position");
+  container.firstElementChild?.classList.add("clone");
+
+  const canva = document.getElementById("canva");
+  if (!canva) return;
+  canva.appendChild(container);
+  window.scrollTo(0, 0);
+}
 </script>
 
 <template>
   <main class="flex flex-col justify-center items-center gap-4">
     <div class="flex flex-col justify-center items-center h-screen gap-6">
       <div
-        class="p-4 outline-dashed outline-4 outline-gray-900 rounded bg-gray-700 grid gap-2"
+        id="canva"
+        class="relative p-4 outline-dashed outline-4 outline-gray-900 rounded bg-gray-700 grid gap-2"
         :style="`grid-template-columns: repeat(${length}, minmax(0, 1fr))`"
       >
         <template v-if="!solutions.length">
           <component
-            v-for="value in _base"
+            v-for="value in base"
             :is="value === 0 ? CanvaSpot : CanvaBlock"
             :block="blockList.find((b) => b.id === value)"
           ></component>
@@ -118,16 +145,18 @@ function solve() {
         <div class="text-xl">Positions</div>
         <div class="flex flex-row items-start flex-wrap gap-6">
           <div
-            v-for="block in selectedBlockPositionList"
-            class="p-3 grid gap-2 w-fit"
-            :style="`grid-template-columns: repeat(${block.x}, minmax(0, 1fr))`"
+            v-for="(position, index) in selectedBlockPositionList"
+            :id="index.toString()"
+            class="p-3 grid gap-2 w-fit rounded position"
+            :style="`grid-template-columns: repeat(${position.x}, minmax(0, 1fr))`"
+            @click="handlePositionSelect($event, index)"
           >
-            <template v-for="el in block.base">
+            <template v-for="el in position.base">
               <div
                 class="w-10 h-10"
                 :class="
                   el === BaseEnum.ENTITY
-                    ? 'rounded bg-gray-800 shadow-lg'
+                    ? 'rounded shadow-lg bg-gray-950'
                     : 'bg-transparent'
                 "
               ></div>
@@ -138,3 +167,12 @@ function solve() {
     </div>
   </main>
 </template>
+
+<style lang="css">
+.position {
+  @apply hover:cursor-pointer hover:outline outline-gray-500 outline-2 hover:bg-gray-600;
+}
+.clone {
+  @apply p-[6px] rounded outline-dashed outline-white outline-[3px] hover:cursor-grab hover:outline-rose-600;
+}
+</style>
