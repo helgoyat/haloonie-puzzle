33e8f17bf593ce51d7f8d1dfc84ff9242caa3101
Helene Goyat <goyat.helene@gmail.com>
2025-05-14 21:49:23 +0200

    Add tip

--- src/App.vue
+++ src/App.vue
@@ -249,7 +249,10 @@ function handleDeleteClone() {
             </li>
             <li>
               Move the puzzle piece on the board using drag-and-drop
-              functionality. You can delete or confirm placement.
+              functionality. You can delete or confirm placement
+              <span class="text-red-500"
+                >(compatibility with all browsers is pending)</span
+              >.
             </li>
             <li>
               Click "Template" button to populate the board with a predefined
